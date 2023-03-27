6f23bdd850799f93639d5be9bd243828517530b3
ChintalaVineetha <vineetha.chintala@isowhiz.in>
2023-03-27 15:02:51 +0530

    code

---
+++ main.py
@@ -0,0 +1,17 @@
+import pdfkit
+
+# set path to input HTML file and output PDF file
+input_file = 'pptemp.html'
+output_file = 'output.pdf'
+
+# configure options for pdfkit
+options = {
+    'page-size': 'Letter',
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in'
+}
+
+# convert HTML file to PDF
+pdfkit.from_file(input_file, output_file, options=options)
